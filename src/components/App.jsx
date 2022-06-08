fb76d625610fa5054b109158ba9827fce3d2dfd6
okolobaha-me <yevheniy.onyshchenko@gmail.com>
2022-06-08 11:08:50 +0300

    added form & title with styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import { Title } from './Title/Title';
+import { Form } from './Form/Form';
+import s from './App.module.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={s.container}>
+      <Title title={'Создание анкеты'} />
+      <Form />
     </div>
   );
 };
